5c9258ebb58f65010dfc35e08182438175a4cd0a
sven-cork <linslus81@gmail.com>
2023-07-16 16:34:33 +0000

    Finalizing for deployment

--- bageriet/settings.py
+++ bageriet/settings.py
@@ -31,7 +31,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['django-bakery-79fee6bf117c.herokuapp.com', 'localhost', '8000-svencork-djangobakery-ueorlxlqytk.ws-eu101.gitpod.io']
 
